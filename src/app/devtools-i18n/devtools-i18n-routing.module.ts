fd309542e90bed8fc3d06019834f816293856913
gnganapath <gnganapathi@gmail.com>
2022-03-15 22:38:21 +0530

    All modules routing aligned with left menu- font changes in global - style.less

--- src/app/devtools-i18n/devtools-i18n-routing.module.ts
+++ src/app/devtools-i18n/devtools-i18n-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevtoolsI18nComponent } from './devtools-i18n.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: DevtoolsI18nComponent,
+  children: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
